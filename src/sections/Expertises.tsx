e338b226488892aa602a6dab26186b790b9264e3
Dylan Dorusse-Repi <agence.drdigital@gmail.com>
2025-01-08 21:30:26 +0100

    correction composant

--- src/sections/Expertises.tsx
+++ src/sections/Expertises.tsx
@@ -23,22 +23,26 @@ const Expertises = () => {
 
     if (error) return <p>{error.message}</p>;
 
-    // @ts-ignore
+
     return (
-        <section className="pb-6 md:pb-8 lg:pb-12">
-            <h2 className="text-h3 font-bold text-center pb-6">{data.expertises.title}</h2>
-            <p className="text-center paragraph max-w-[49rem] mx-auto pb-12 whitespace-pre-line">{data.expertises.description}</p>
-            <ScrollTrigger onEnter={() => setCounterStart(true)}>
-            <div className="bg-accent rounded-lg grid grid-cols-2 lg:grid-cols-4 w-fit mx-auto gap-8 p-8 text-white">
-                {data.expertises.counter.map((item: ExpertiseType) => (
-                    <div className="flex flex-col items-center justify-center px-8 text-center" key={item.id}>
-                        <span className="font-bold text-h3">{counterStart && <CountUp start={0} duration={1.5} end={item.number}/>}</span>
-                        <span>{item.expertise}</span>
+        <>
+            {data.expertises && <section className="pb-6 md:pb-8 lg:pb-12">
+                <h2 className="text-h3 font-bold text-center pb-6">{data.expertises.title}</h2>
+                <p className="text-center paragraph max-w-[49rem] mx-auto pb-12 whitespace-pre-line">{data.expertises.description}</p>
+                <ScrollTrigger onEnter={() => setCounterStart(true)}>
+                    <div
+                        className="bg-accent rounded-lg grid grid-cols-2 lg:grid-cols-4 w-fit mx-auto gap-8 p-8 text-white">
+                        {data.expertises.counter.map((item: ExpertiseType) => (
+                            <div className="flex flex-col items-center justify-center px-8 text-center" key={item.id}>
+                                <span className="font-bold text-h3">{counterStart &&
+                                    <CountUp start={0} duration={1.5} end={item.number}/>}</span>
+                                <span>{item.expertise}</span>
+                            </div>
+                        ))}
                     </div>
-                ))}
-            </div>
-            </ScrollTrigger>
-        </section>
+                </ScrollTrigger>
+            </section>}
+        </>
     );
 };
 
