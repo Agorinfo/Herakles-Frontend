cad5f419de523111b94efe5e0f3574926d37ea26
Dylan <agence.drdigital@gmail.com>
2026-04-10 14:46:03 +0200

    alt logo

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -7,7 +7,6 @@ import getGlobal from "@/actions/getGlobal";
 import getMenu from "@/actions/getMenu";
 import Loader from "@/components/Loader";
 import Link from "next/link";
-import Modal from "@/components/Modal";
 import ContactForm from "@/components/ContactForm";
 import emptyImg from "@/assets/empty-img.png"
 
@@ -25,6 +24,7 @@ const Header = () => {
     })
 
     const logo = global.data?.logo.data?.attributes.url
+    const alt = global.data?.logo.data?.attributes.alternativeText
     if(isLoading) return  <Loader />
 
     if(error) return <p>{error.message}</p>
@@ -32,7 +32,7 @@ const Header = () => {
     return (
         <header className="sticky top-0 bg-white z-[999] border-b border-greyscale-lightest shadow-nav">
             <div className="flex items-center justify-between py-6 relative max-w-screen-xl mx-auto px-4 md:px-8 xl:px-0">
-                <Link className="w-[200px]" href="/"><img className="w-full h-full object-contain" src={logo ? backUrl + logo : emptyImg.src} alt="WeNégoce"/></Link>
+                <Link className="w-[200px]" href="/"><img className="w-full h-full object-contain" src={logo ? backUrl + logo : emptyImg.src} alt={alt}/></Link>
                 <Nav navItems={data.navItems} isOpen={isOpen} setIsOpen={setIsOpen} />
                 <ModalButton label={"Nous contacter"} className="btn btn-accent hidden lg:inline-flex">
                     <ContactForm />
